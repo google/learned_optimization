c2a2141566a226b231d4df156932f87365bfaaad
Luke Metz <lmetz@google.com>
2022-01-22 11:28:09 -0800

    Initial task augmentations based on reparametrizing weights.
    PiperOrigin-RevId: 423544553

---
+++ learned_optimization/tasks/task_augmentation.py
@@ -0,0 +1,180 @@
+# coding=utf-8
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Augmentations which transform Task and TaskFamily.
+
+In supervised learning, augmentations are automatic modifications to training
+data used to encourage generalization. When training learned optimizers, we
+seek to do a similar set of transformations but these transformations now must
+operate on Task and TaskFamiliy resulting in new Task and TaskFamily.
+"""
+
+from typing import Mapping, Union, Tuple
+
+import gin
+import jax
+import jax.numpy as jnp
+from learned_optimization import summary
+from learned_optimization.tasks import base
+from learned_optimization.tasks.base import Batch, ModelState, Params  # pylint: disable=g-multiple-import
+from learned_optimization.tasks.parametric import cfgobject
+import numpy as onp
+
+PRNGKey = jnp.ndarray
+
+
+class ReparamWeights(base.Task):
+  """Reparameterize weights of target task by the param_scale.
+
+  If the underlying loss is f(x;w) = w@x, this function transforms the loss to
+  be f(x;w) = (w*param_scale)@x and changes the initial weights to be:
+  w=w0/param_scale where w0 is the provided Task's init.
+
+  This reparameterization does NOT change the underlying function, but does
+  change the learning dynamics of the problem greatly as the underlying params
+  will be more, or less sensitive.
+  """
+
+  def __init__(self, task: base.Task, param_scale: Union[Params, float]):
+    super().__init__()
+    self.task = task
+    self.normalizer = task.normalizer
+    self.datasets = task.datasets
+    self._param_scale = param_scale
+
+  def _match_param_scale_to_pytree(self, params: Params) -> Params:
+    if isinstance(self._param_scale, (jnp.ndarray, onp.ndarray, float, int)):
+      return jax.tree_map(lambda x: self._param_scale, params)
+    else:
+      tree = jax.tree_structure(params)
+      tree_scale = jax.tree_structure(self._param_scale)
+      assert tree == tree_scale
+      return self._param_scale
+
+  def init_with_state(self, key: PRNGKey) -> Tuple[Params, ModelState]:
+    params, state = self.task.init_with_state(key)
+    scales = self._match_param_scale_to_pytree(params)
+    params = jax.tree_map(lambda x, scale: x / scale, params, scales)
+    return params, state
+
+  def init(self, key: PRNGKey) -> Params:
+    params, _ = self.init_with_state(key)
+    return params
+
+  def loss_with_state_and_aux(
+      self, params: Params, state: ModelState, key: PRNGKey,
+      data: Batch) -> Tuple[jnp.ndarray, ModelState, Mapping[str, jnp.ndarray]]:
+    scales = self._match_param_scale_to_pytree(params)
+    params = jax.tree_map(lambda x, scale: x * scale, params, scales)
+    return self.task.loss_with_state_and_aux(params, state, key, data)
+
+  def loss(self, params: Params, key: PRNGKey, data: Batch) -> jnp.ndarray:
+    loss, _, _ = self.loss_with_state_and_aux(params, None, key, data)
+    return loss
+
+
+@gin.configurable
+class ReparamWeightsFamily(base.TaskFamily):
+  """Reparam the weights of a TaskFamily.
+
+  This family resamples the weights of the reparameterization for each different
+  task sample. Within a task samples the parameter scale remains constant.
+
+  There are 3 possible levels of this parameterization:
+    global: Parameters get scaled with the same sampled value.
+    tensor: Parameters get scaled with a separate value per tensor.
+    parameter: Parameters get scaled with a separate value per parameter.
+
+  See `ReparamWeights` for how this reparameterization is done.
+  """
+
+  def __init__(self,
+               task_family: base.TaskFamily,
+               level: str,
+               param_scale_range: Tuple[float, float] = (0.01, 100.)):
+    super().__init__()
+    assert level in ["global", "tensor", "parameter"]
+    self._level = level
+    self.task_family = task_family
+    self._param_scale_range = tuple(param_scale_range)
+    self.datasets = task_family.datasets
+
+  def _single_random(self, key):
+    min_val, max_val = self._param_scale_range
+    param_scale = jax.random.uniform(
+        key, [], minval=jnp.log(min_val), maxval=jnp.log(max_val))
+    return jnp.exp(param_scale)
+
+  def task_fn(self, cfg: cfgobject.CFGNamed) -> base.Task:
+    if self._level in ["global", "tensor"]:
+      sub_config, param_scale = cfg.values["sub_cfg"], cfg.values["param_scale"]
+
+    if self._level == "parameter":
+      sub_config, keys = cfg.values["sub_cfg"], cfg.values["param_scale_keys"]
+      abstract_params = jax.eval_shape(
+          lambda key: self.task_family.sample_task(key).init(key),
+          jax.random.PRNGKey(0))
+
+      def single(p, key):
+        min_val, max_val = self._param_scale_range
+        param_scale = jax.random.uniform(
+            key, p.shape, minval=jnp.log(min_val), maxval=jnp.log(max_val))
+        return jnp.exp(param_scale)
+
+      param_scale = jax.tree_map(single, abstract_params, keys)
+
+    task = self.task_family.task_fn(sub_config)
+    return ReparamWeights(task, param_scale)
+
+  def sample(self, key: PRNGKey) -> cfgobject.CFGNamed:
+    key1, key2 = jax.random.split(key)
+    sub_config = self.task_family.sample(key1)
+
+    if self._level == "global":
+      param_scale = self._single_random(key2)
+      summary.summary(
+          "ReparamWeightsFamily/global_param_scale",
+          param_scale,
+          aggregation="sample")
+      return cfgobject.CFGNamed("ReparamWeightsFamily", {
+          "sub_cfg": sub_config,
+          "param_scale": param_scale
+      })
+    elif self._level == "tensor":
+      abstract_params = jax.eval_shape(
+          lambda key: self.task_family.sample_task(key).init(key),
+          jax.random.PRNGKey(0))
+      leaves, tree = jax.tree_flatten(abstract_params)
+      keys = jax.tree_unflatten(tree, jax.random.split(key2, len(leaves)))
+      param_scale = jax.tree_multimap(self._single_random, keys)
+      return cfgobject.CFGNamed("ReparamWeightsFamily", {
+          "sub_cfg": sub_config,
+          "param_scale": param_scale
+      })
+    elif self._level == "parameter":
+      # the task configs cannot be huge as they are saved to disk and logged.
+      # As such, for per parameter applications a rng key is used instead
+      # of the generated value.
+      abstract_params = jax.eval_shape(
+          lambda key: self.task_family.sample_task(key).init(key),
+          jax.random.PRNGKey(0))
+      leaves, tree = jax.tree_flatten(abstract_params)
+      keys = jax.tree_unflatten(tree, jax.random.split(key2, len(leaves)))
+      return cfgobject.CFGNamed("ReparamWeightsFamily", {
+          "sub_cfg": sub_config,
+          "param_scale_keys": keys,
+      })
+    else:
+      raise ValueError(f"level={self._level} not supported")
---
+++ learned_optimization/tasks/task_augmentation_test.py
@@ -0,0 +1,137 @@
+# coding=utf-8
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for task_augmentation."""
+
+from typing import Any, Tuple
+
+from absl.testing import absltest
+import jax
+import jax.numpy as jnp
+from learned_optimization.tasks import base
+from learned_optimization.tasks import task_augmentation
+import numpy as onp
+from numpy import testing
+
+
+class DummyTask(base.Task):
+
+  def init(self, key: jnp.ndarray) -> Any:
+    return (jnp.ones([2]), jnp.ones([2]))
+
+  def loss(self, params: Any, key: jnp.ndarray,
+           data: Any) -> Tuple[jnp.ndarray, Any]:
+    return jnp.sum(params[0] + params[1])
+
+
+class DummyTaskFamily(base.TaskFamily):
+
+  def sample(self, key: jnp.ndarray) -> Any:
+    return {'cfg': 123}
+
+  def task_fn(self, cfg: Any) -> base.Task:
+    return DummyTask()
+
+
+class TaskAugmentationTest(absltest.TestCase):
+
+  def test_ReparamWeight(self):
+    base_task = DummyTask()
+    key = jax.random.PRNGKey(0)
+
+    # One common weight
+    task = task_augmentation.ReparamWeights(base_task, 2.0)
+    weights = task.init(key)
+    testing.assert_allclose(weights[0], onp.ones([2]) / 2.0)
+    testing.assert_allclose(weights[1], onp.ones([2]) / 2.0)
+    l = task.loss(weights, key, None)
+    testing.assert_allclose(l, 4.0)
+
+    # Separate weights per param vector
+    task = task_augmentation.ReparamWeights(base_task, (2.0, 0.5))
+    weights = task.init(key)
+    testing.assert_allclose(weights[0], onp.ones([2]) / 2.0)
+    testing.assert_allclose(weights[1], onp.ones([2]) * 2.0)
+    l = task.loss(weights, key, None)
+    testing.assert_allclose(l, 4.0)
+
+    # Separate weights for one parameter
+    task = task_augmentation.ReparamWeights(base_task,
+                                            (jnp.asarray([2.0, 0.5]), 0.5))
+    weights = task.init(key)
+    testing.assert_allclose(weights[0], onp.asarray([0.5, 2.0]))
+    testing.assert_allclose(weights[1], onp.ones([2]) * 2.0)
+    l = task.loss(weights, key, None)
+    testing.assert_allclose(l, 4.0)
+
+  def test_ReparamWeightsFamily_global(self):
+    base_task_family = DummyTaskFamily()
+    task_family = task_augmentation.ReparamWeightsFamily(
+        base_task_family, 'global', (1.0, 100.0))
+    key = jax.random.PRNGKey(0)
+    task = task_family.sample_task(key)
+    params0 = task.init(key)
+
+    a, b = params0[0]
+    self.assertEqual(a, b)
+
+    c, _ = params0[1]
+    self.assertEqual(a, c)
+
+    key = jax.random.PRNGKey(1)
+    task = task_family.sample_task(key)
+    params1 = task.init(key)
+
+    self.assertGreater(onp.sum((params0[0] - params1[0])**2), 0.001)
+
+  def test_ReparamWeightsFamily_tensor(self):
+    base_task_family = DummyTaskFamily()
+    task_family = task_augmentation.ReparamWeightsFamily(
+        base_task_family, 'tensor', (0.001, 10.0))
+    key = jax.random.PRNGKey(0)
+    task = task_family.sample_task(key)
+    params0 = task.init(key)
+    a, b = params0[0]
+    self.assertEqual(a, b)
+    c, d = params0[1]
+    self.assertEqual(c, d)
+    self.assertGreater((a - c)**2, 0.001)
+
+  def test_ReparamWeightsFamily_parameter(self):
+    base_task_family = DummyTaskFamily()
+    task_family = task_augmentation.ReparamWeightsFamily(
+        base_task_family, 'parameter', (0.001, 10.0))
+    key = jax.random.PRNGKey(0)
+    task = task_family.sample_task(key)
+    params0 = task.init(key)
+    a, b = params0[0]
+    self.assertGreater((a - b)**2, 0.001)
+    c, d = params0[1]
+    self.assertGreater((c - d)**2, 0.001)
+
+    # Randomness is sampled at the task construction level so these should be
+    # equal
+    params1 = task.init(jax.random.PRNGKey(1))
+    a1, _ = params1[0]
+    self.assertEqual(a, a1)
+
+    # Different samples of the task should be different
+    params2 = task_family.sample_task(jax.random.PRNGKey(1)).init(key)
+    a2, _ = params2[0]
+    self.assertGreater((a - a2)**2, 0.001)
+
+
+if __name__ == '__main__':
+  absltest.main()
